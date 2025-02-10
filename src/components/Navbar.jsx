d2a6a971b53390cc36375f489cea665ea44e4f6e
Mehul Gupta <mehulguptar.w453@gmail.com>
2025-02-11 00:37:08 +0530

    `Update author name in Navbar component`

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Vansh &nbsp;
+            Mehul &nbsp;
             <span className='sm:block hidden'> | MERN Stack Developer</span>
           </p>
         </Link>
